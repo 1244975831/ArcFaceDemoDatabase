223a3113ce74101dac82fdeb0e9140e575a96a61
1244975831 <1244975831@qq.com>
2020-05-18 18:43:05 +0800

    Update Constants.java

--- app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -2,8 +2,8 @@ package com.arcsoft.arcfacedemo.common;
 
 public class Constants {
 
-    public static final String APP_ID = "D617np8jyKt1jN9gMr7ENbW1aMgEPVEtz6jiJSgwomrs";
-    public static final String SDK_KEY = "F4EZmmiqs3BBce5pPnKbxJ6czCSuXsKpPnghdgHntG2k";
+    public static final String APP_ID = "官网获取";
+    public static final String SDK_KEY = "官网获取";
 
     /**
      * IR预览数据相对于RGB预览数据的横向偏移量，注意：是预览数据，一般的摄像头的预览数据都是 width > height
